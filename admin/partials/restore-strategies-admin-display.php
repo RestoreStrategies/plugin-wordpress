2c74295d3e15b9ff115dea6df8f17098d19760a1
Joseph Simmons <josephdsimmons@gmail.com>
2016-08-23 12:33:48 -0500

    Adds instructions on admin setup page [#128529963]

--- admin/partials/restore-strategies-admin-display.php
+++ admin/partials/restore-strategies-admin-display.php
@@ -19,6 +19,41 @@
     <h1>Restore Strategies Settings</h1>
 
 
+    <p>To setup the Restore Strategies plugin, provide your API credentials & create a signup page (a page with the <b>[restore-strategies-signup-form]</b> shortcode on it).</p>
+
+    <p>Then, feel free to use Restore Strategies’ shortcodes throughout your site. Four shortcodes are available.</p>
+
+    <h2>[restore-strategies-opportunity]</h2>
+
+    <p>This shortcode displays a single opportunity based on it’s id. For example,<br />
+    <kbd>[restore-strategies-opportunity id="511"]</kbd></p>
+    
+    <h2>[restore-strategies-signup-form]</h2>
+    
+    <p>This shortcode creates a signup form. It takes no parameters & must be used on at least one page for the plugin to work correctly.</p>
+    
+    <h2>[restore-strategies-search-box]</h2>
+    
+    <p>This shortcode creates a search box. The search box can be simple, with just a text input field, or advanced with several categories of check boxes. If you would like every search to be prefixed with a certain term, you can provide that. For example,<br />
+    <kbd>[restore-strategies-search-box advanced="yes" prefix="foster care"]</kbd></p>
+    
+    <h2>[restore-strategies-search]</h2>
+    
+    <p>This shortcode does a search & returns the results. It's the fastest way to list several opportunities on a page if they can all be found from a single search. It's possible to do an advanced search with several different parameters. The possible parameters are listed below:</p>
+    
+    <kbd>[restore-strategies-search<br />
+        &nbsp;&nbsp;q="&lt;search term&gt;"<br />
+        &nbsp;&nbsp;issues="&lt;issue 1&gt;,&lt;issue 2&gt;,..."<br />
+        &nbsp;&nbsp;regions="&lt;region 1&gt;,&lt;region 2&gt;,..."<br />
+        &nbsp;&nbsp;times="&lt;time 1&gt;,&lt;time 2&gt;,..."<br />
+        &nbsp;&nbsp;days="&lt;day 1&gt;,&lt;day 1&gt;,..."<br />
+        &nbsp;&nbsp;type="&lt;opportunity type 1&gt;,&lt;opportunity type 2&gt;,..."<br />
+        &nbsp;&nbsp;group_type="&lt;group type 1&gt;,&lt;group type 2&gt;,..."<br />
+    ]</kbd>
+    
+    <p>For example:</p>
+    
+    <kbd>[restore-strategies-search q="foster care" issues="Children/Youth,Sports" regions="North,Central" times="Morning,Evening" days="Monday,Thursday" type="Service,Training" group_type="Individual,Group"]</kbd>
     <form method="post" action="options.php">
         <table class="form-table">
             <?php
@@ -39,7 +74,7 @@
 
             <tr>
                 <th scope="row">
-                    <label for="token">API Token</label>
+                    <label for="token">API Token*</label>
                 </th>
                 <td>
                     <input
@@ -48,13 +83,14 @@
                         value="<?php if(!empty($token)) { echo $token; } ?>"
                         type="text"
                         class="regular-text"
+                        required
                     />
                 </td>
             </tr>
 
             <tr>
                 <th scope="row">
-                    <label for="secret">API Secret</label>
+                    <label for="secret">API Secret*</label>
                 </th>
                 <td>
                     <input
@@ -63,13 +99,14 @@
                         value="<?php if(!empty($secret)) { echo $secret; } ?>"
                         type="text"
                         class="regular-text"
+                        required
                     />
                 </td>
             </tr>
 
             <tr>
                 <th scope="row">
-                    <label for="token">Signup page URL</label>
+                    <label for="token">Signup page URL*</label>
                 </th>
                 <td>
                     <input
@@ -78,8 +115,9 @@
                         value="<?php if(!empty($signup_url)) { echo $signup_url; } ?>"
                         type="text"
                         class="regular-text"
+                        required
                     />
-                    <span>The relative URL of the page you want to use for the signup form.</span>
+                    <span>The URL of the page you put the sign-up form on (you need to create this page).</span>
                 </td>
             </tr>
 
@@ -95,7 +133,7 @@
                         value="1"
                         <?php checked($include_church, 1); ?>
                     />
-                    <span>If you'd like volunteers to indicate which church they're a part of.</span>
+                    <span>Include a field for church on the sign-up form. If not included, the church your API user is associated with will automatically be used.</span>
                 </td>
             </tr>
 
@@ -111,7 +149,7 @@
                         value="1"
                         <?php checked($include_campus, 1); ?>
                     />
-                    <span>If you'd like volunteers to indicate which church campus they're a part of.</span>
+                    <span>Include a field for church campus on the sign-up form.</span>
                 </td>
             </tr>
 
@@ -120,7 +158,7 @@
                     <label for="secret">Custom confirmation message</label>
                 </th>
                 <td>
-                    <p>If you would like to include a custom signup confirmation message, put it here. This message will appear above the opportunity's specific confirmation message.</p><br />
+                    <p>Optionally, include a message that will appear on the sign-up confirmation page. The default message is "Thank you for signing up!"</p><br />
                     <textarea
                         name="<?php echo $this->plugin_name; ?>[confirmation_message]"
                         id="<?php echo $this->plugin_name; ?>-confirmation_message"
@@ -132,7 +170,7 @@
 
             <tr>
                 <th scope="row">
-                    <label for="instructions">Hide specific confirmation message</label>
+                    <label for="instructions">Hide post-signup instructions</label>
                 </th>
                 <td>
                     <input
@@ -142,7 +180,7 @@
                         value="1"
                         <?php checked($hide_message, 1); ?>
                     />
-                    <span>Check this if you do not want the opportunity's additional signup instructions to appear on the signup confirmation page.</span>
+                    <span>Do not display opportunity specific next step instructions on the sign-up confirmation page.</span>
                 </td>
             </tr>
 
