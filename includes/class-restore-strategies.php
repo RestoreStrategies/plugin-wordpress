1937939196a36feada601821f626f5e09b5f2891
Joseph Simmons <josephdsimmons@gmail.com>
2017-04-04 11:07:29 -0500

    Bumps version in Restore_Strategies class

--- includes/class-restore-strategies.php
+++ includes/class-restore-strategies.php
@@ -69,7 +69,7 @@ class Restore_Strategies {
 	public function __construct() {
 
 		$this->plugin_name = 'restore-strategies';
-		$this->version = '1.0.0';
+		$this->version = '1.0.2';
 
 		$this->load_dependencies();
 		$this->set_locale();
