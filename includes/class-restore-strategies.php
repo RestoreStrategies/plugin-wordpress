738d894690d20b1b1b01c1d87d0f1e881bc5c18a
Joseph Simmons <josephdsimmons@gmail.com>
2017-05-23 12:12:45 -0500

    Version bump
    Updates PHP client with /api/search? 401 bug fix
    Sets user agent

--- includes/class-restore-strategies.php
+++ includes/class-restore-strategies.php
@@ -69,7 +69,7 @@ class Restore_Strategies {
 	public function __construct() {
 
 		$this->plugin_name = 'restore-strategies';
-		$this->version = '1.0.4';
+		$this->version = '1.0.5';
 
 		$this->load_dependencies();
 		$this->set_locale();
